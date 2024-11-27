8a7a308ab8413a187cd276aab2d487f937558e50
Jaskaran1519 <jaskaran1519@gmail.com>
2024-11-27 17:17:02 +0530

    sfslfn

--- app/_components/ModelViewer.jsx
+++ app/_components/ModelViewer.jsx
@@ -55,7 +55,7 @@ const ModelViewer = ({
   }, [modelUrl]);
 
   return (
-    <div className="relative w-[90%] h-screen mx-auto">
+    <div className="relative w-[90%] h-screen mx-auto my-auto rounded-lg">
       {/* AR Button in Top Right Corner */}
       {/* {isArSupported && (
         <button 
